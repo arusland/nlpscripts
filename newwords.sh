86594d608d655be97795547f3f2357fd27de3b17
Ruslan A <ruslan.absalyamov@gmail.com>
2023-04-23 19:12:50 +0200

    Fix newwords.sh

--- newwords.sh
+++ newwords.sh
@@ -6,9 +6,9 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
-cat "$1" | tr 'A-Z' 'a-z' | tr -d [:punct:] |  tr -d [:digit:] | tr -sc 'a-z' '\12' | fgrep -vwf "$2" | sort | uniq
+cat "$1" | sed 's/<[^>]*>/ /g' | tr 'A-Z' 'a-z' | tr -d [:punct:] |  tr -d [:digit:] | tr -sc 'a-z' '\12' | fgrep -vwf "$2" | sort | uniq
 
-
+# cat "$1" | sed 's/<[^>]*>/ /g' # clean html tags
 # cat input.srt | tr 'A-Z' 'a-z' | tr -d [:punct:] |  tr -d [:digit:] > input_cleaned.srt 
 # cat input_cleaned.srt | tr -sc 'a-z' '\12'> input_tokens.txt
 # cat input_tokens.txt | fgrep -vwf known_words_en.txt | sort | uniq > new_words.txt
