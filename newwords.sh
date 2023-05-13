ae10e81c707917dd6252f5f8b0f1cbb6e0e57363
Ruslan A <ruslan.absalyamov@gmail.com>
2023-05-13 11:53:43 +0200

    Add support German

--- newwords.sh
+++ newwords.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script can be useful when you only need to extract new words from any subtitle/book file
+
 
 if [ -z "$2" ]; then
     echo "Extract new words from raw text file and by stop words file"
@@ -6,9 +8,21 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
+if [ "$3" == "de" ]; then
+    # German
+    cat "$1" | sed 's/<[^>]*>/ /g' | sed -r 's/^([A-ZÄÖÜ])/\L\1/g' | sed -r 's/([-.?!]\s+)([A-ZÄÖÜ])/\1\L\2/g' \
+    | tr -d [:punct:] |  tr -d [:digit:] | tr -sc 'A-ZÄÖÜa-zäöüß' '\12' | fgrep -vwf "$2" | sort | uniq
+    exit 0
+fi
+
+# English
 cat "$1" | sed 's/<[^>]*>/ /g' | tr 'A-Z' 'a-z' | tr -d [:punct:] |  tr -d [:digit:] | tr -sc 'a-z' '\12' | fgrep -vwf "$2" | sort | uniq
 
 # cat "$1" | sed 's/<[^>]*>/ /g' # clean html tags
-# cat input.srt | tr 'A-Z' 'a-z' | tr -d [:punct:] |  tr -d [:digit:] > input_cleaned.srt 
-# cat input_cleaned.srt | tr -sc 'a-z' '\12'> input_tokens.txt
-# cat input_tokens.txt | fgrep -vwf known_words_en.txt | sort | uniq > new_words.txt
+# cat input.srt | tr 'A-Z' 'a-z' | tr -d [:punct:] |  tr -d [:digit:] # clean text from punct. signs and digits
+# cat input_cleaned.srt | tr -sc 'a-z' '\12' # get separate tokens
+# cat input_tokens.txt | fgrep -vwf known_words_en.txt | sort | uniq # final result
+
+# German related
+# sed -r 's/^([A-ZÄÖÜ])/\L\1/g' # lower case first leters at the begining of sentence
+# sed -r 's/([-.?!]\s+)([A-ZÄÖÜ])/\1\L\2/g' # # lower case first leters at the begining of sentence
