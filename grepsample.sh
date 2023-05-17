9475570c830ac75879c009dda60b418626f343c5
Ruslan A <ruslan.absalyamov@gmail.com>
2023-05-18 00:29:15 +0200

    Add grepsample.sh - find first matched line for each line from the file

---
+++ grepsample.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# output first matched line from second file for each line from first file
+
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <file1> <file2>"
+  echo "output first matched line from second file for each line from first file"
+  echo "Example: $0 file1 file2 | column -t -l 2"
+  exit 1
+fi
+
+file1=$1
+file2=$2
+
+while read -r line; do
+  sample=`grep -i "\b$line\b" $file2 | head -1 | sed 's/<[^>]*>/ /g' | sed 's/^ *//g'`
+  echo -e "$line:\t$sample"
+done < "$file1"
